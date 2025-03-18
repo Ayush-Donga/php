a08e5cab7b7a4910fee2a58526b90609cca71b43
Ayush Donga <ayush@saasinnova.com>
2025-03-18 16:01:29 +0530

    file structure change.

--- what-is-php.php
+++ what-is-php.php
@@ -6,7 +6,8 @@
     <title>What is php ?</title>
 </head>
 <body>
-    <h1>What is php?</h1>
-    <h2> -> PHP (Hypertext Preprocessor) is a server-side scripting language used for web development to create dynamic and interactive web pages.</h2>
+    <h1>(1) What is php?</h1>
+    <h2> => PHP (Hypertext Preprocessor) is a server-side scripting language used for web development to 
+        create dynamic and interactive web pages.</h2>
 </body>
 </html>
